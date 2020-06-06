e52e9cd0f55c3a96291cc9fe32d419989166e832
bradleyhewitt <40677305+bradleyhewitt@users.noreply.github.com>
2020-06-06 15:50:49 -0700

    Attempts

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
-var pgtitleformat = "bradley test hewitt - ";
+var pgtitleformat = "bradley hewitt - ";
 document.title = pgtitleformat + pgtitle;
+
+var terminaltitle = pgtitle + " — -bash"
+
+var prompt = "Bradleys-Portfolio:~ " + pgtitle + "$ █"
+
+document.getElementById("header-title").innerHTML = terminaltitle;
+document.getElementById("prompt").innerHTML = prompt;
