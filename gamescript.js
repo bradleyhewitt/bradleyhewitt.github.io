7fa6d1f78c98b68f434a8430988e6b95212332a7
bradleyhewitt <40677305+bradleyhewitt@users.noreply.github.com>
2021-05-17 21:33:11 -0700

    Fixes

--- gamescript.js
+++ gamescript.js
@@ -11,6 +11,8 @@ function makeLandscape(){
 }
 
 function makePortrait(){
+	var maxHeight = window.innerHeight * 0.75;
+	$("body").css("max-height", maxHeight.toString());
 	$(".textdisplay").css("max-width", "80vw");
 	$(".textdisplay").css("width", "80vw");
 	$(".textdisplay").css("font-size", "50px");
