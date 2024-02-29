06dbb726dbe6f9dbbd0c9e10d67e78fda88c0aff
bradleyhewitt <bradleywh01@gmail.com>
2024-02-29 14:47:33 -0800

    Add icons

--- src/resume/Resume.tsx
+++ src/resume/Resume.tsx
@@ -1,4 +1,5 @@
 import "./Resume.sass"
+import {MdOutlineOpenInNew} from "react-icons/md";
 
 export default function Resume() {
   return (
@@ -8,10 +9,7 @@ export default function Resume() {
           RÉSUMÉ
         </h1>
         <a href={"https://raw.githubusercontent.com/bradleyhewitt/bradleyhewitt.github.io/master/src/assets/resume.pdf"} className={"svglink"} target={"_blank"} rel={"noopener noreferrer"}>
-          <svg width="24" height="24" viewBox="0 0 24 24" strokeWidth="1.5" fill="none">
-            <path d="M21 3L15 3M21 3L12 12M21 3V9" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round"/>
-            <path d="M21 13V19C21 20.1046 20.1046 21 19 21H5C3.89543 21 3 20.1046 3 19V5C3 3.89543 3.89543 3 5 3H11" stroke="currentColor" strokeLinecap="round"/>
-          </svg>
+          <MdOutlineOpenInNew size={24} />
         </a>
       </div>
       <img className={"pdf"} src={"https://raw.githubusercontent.com/bradleyhewitt/bradleyhewitt.github.io/master/src/assets/resume.svg"} alt={"RÉSUMÉ"}/>
