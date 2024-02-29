12f18fc68d0a60f88be1160385344afb09adebe3
bradleyhewitt <bradleywh01@gmail.com>
2024-02-29 14:41:06 -0800

    Add icons

--- src/projects/Project.tsx
+++ src/projects/Project.tsx
@@ -1,9 +1,16 @@
-export default function Project(props: { title: string, desc: string, url: string }) {
+import {ReactNode} from "react";
+
+export default function Project(props: { icon: ReactNode, title: string, desc: string, url: string }) {
   return (
       <div className={"project"} onClick={() => {
         window.open(props.url, "_blank", "noreferrer")
       }}>
-        <div className={"project-title"}><h1>{props.title}</h1></div>
+        <div className={"project-title"}>
+          <div className={"project-icon"}>
+            {props.icon}
+          </div>
+          <h1>{props.title}</h1>
+        </div>
         <p className={"project-description"}>{props.desc}</p>
       </div>
     );
--- src/projects/Projects.tsx
+++ src/projects/Projects.tsx
@@ -1,5 +1,10 @@
 import Project from "./Project.tsx";
 import "./Projects.sass"
+import {
+  MdOutlineHome,
+  MdOutlineLocalPizza,
+  MdOutlineSportsBasketball
+} from "react-icons/md";
 
 export default function Projects() {
   return (
@@ -9,16 +14,19 @@ export default function Projects() {
       </h1>
       <div className={"projects"}>
         <Project
+          icon={<MdOutlineSportsBasketball size={18} />}
           title={"Shport"}
           desc={"Web application for following sports teams or leagues — providing schedules, news, standings, and real-time scores."}
           url={"https://github.com/bradleyhewitt/Shport"}
         />
         <Project
+          icon={<MdOutlineLocalPizza size={18} />}
           title={"Delightful"}
           desc={"Minecraft Java modification focused on extending the ecosystem with a more cohesive meal selection. It has amassed over eight million downloads and continues to grow."}
           url={"https://github.com/brnbrd/Delightful"}
         />
         <Project
+          icon={<MdOutlineHome size={18} />}
           title={"This Page"}
           desc={"Portfolio page written in React.js."}
           url={"https://github.com/bradleyhewitt/bradleyhewitt.github.io"}
